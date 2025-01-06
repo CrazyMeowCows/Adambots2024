99406e7e103ea81f172cd1aafd764ad42b68ebcc
Tommy R <drumming.dog101@gmail.com>
2025-01-05 23:34:01 -0500

    first commit

---
+++ src/main/java/com/adambots/RobotMap.java
@@ -0,0 +1,86 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.adambots;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.adambots.Constants.DriveConstants;
+import com.adambots.Constants.ModuleConstants;
+import com.adambots.Constants.DriveConstants.ModulePosition;
+import com.adambots.sensors.Gyro;
+import com.adambots.subsystems.SwerveModule;
+
+/**
+ * Define all the devices here
+ */
+public class RobotMap {
+    // PORTS Definition - This should be the only place to define all ports
+    public static final int kRearLeftTurningEncoderPort = 2;
+    public static final int kRearRightTurningEncoderPort = 3;
+    public static final int kFrontLeftTurningEncoderPort = 4;
+    public static final int kFrontRightTurningEncoderPort = 5;
+
+    public static final int kFrontRightTurningMotorPort = 13;
+    public static final int kFrontRightDriveMotorPort = 11;
+
+    public static final int kRearRightTurningMotorPort = 18;
+    public static final int kRearRightDriveMotorPort = 16;
+
+    public static final int kRearLeftTurningMotorPort = 14;
+    public static final int kRearLeftDriveMotorPort = 12;
+    
+    public static final int kFrontLeftTurningMotorPort = 15;
+    public static final int kFrontLeftDriveMotorPort = 17;        
+
+    // Operator Interface (Joystick and XBoxControllers)
+    public static final int kJoystickControllerPort = 0;
+    public static final int kPrimaryControllerPort = 1; // XBOX Controller
+    public static final int kSecondaryControllerPort = 2; // XBOX Controller
+
+    // Robot Devices and Sensors
+    public static final Gyro gyro = new Gyro(0);
+
+    // Robot Swerve Modules
+    public static final HashMap<ModulePosition, SwerveModule> swerveModules = new HashMap<>(
+        Map.of(
+            ModulePosition.FRONT_LEFT,
+            new SwerveModule(
+                    ModulePosition.FRONT_LEFT,
+                    RobotMap.kFrontLeftDriveMotorPort,
+                    RobotMap.kFrontLeftTurningMotorPort,
+                    RobotMap.kFrontLeftTurningEncoderPort,
+                    DriveConstants.kFrontLeftDriveMotorReversed,
+                    ModuleConstants.kFrontLeftEncoderOffset),
+            ModulePosition.FRONT_RIGHT,
+            new SwerveModule(
+                    ModulePosition.FRONT_RIGHT,
+                    RobotMap.kFrontRightDriveMotorPort,
+                    RobotMap.kFrontRightTurningMotorPort,
+                    RobotMap.kFrontRightTurningEncoderPort,
+                    DriveConstants.kFrontRightDriveMotorReversed,
+                    ModuleConstants.kFrontRightEncoderOffset),
+            ModulePosition.REAR_LEFT,
+            new SwerveModule(
+                    ModulePosition.REAR_LEFT,
+                    RobotMap.kRearLeftDriveMotorPort,
+                    RobotMap.kRearLeftTurningMotorPort,
+                    RobotMap.kRearLeftTurningEncoderPort,
+                    DriveConstants.kRearLeftDriveMotorReversed,
+                    ModuleConstants.kRearLeftEncoderOffset),
+            ModulePosition.REAR_RIGHT,
+            new SwerveModule(
+                    ModulePosition.REAR_RIGHT,
+                    RobotMap.kRearRightDriveMotorPort,
+                    RobotMap.kRearRightTurningMotorPort,
+                    RobotMap.kRearRightTurningEncoderPort,
+                    DriveConstants.kRearRightDriveMotorReversed,
+                    ModuleConstants.kRearRightEncoderOffset)     
+        )
+    );
+}
---
+++ src/main/java/com/adambots/sensors/AbsoluteEncoder.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.adambots.sensors;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * Generic Absolute Encoder sensor to hide actual implementation and ensure uniform values across subsystems
+ */
+public class AbsoluteEncoder {
+    private AnalogInput encoder;
+    private double offset;
+    
+    public AbsoluteEncoder (int port, double offset){
+        this.encoder = new AnalogInput(port); //Defining the encoder using the port passed in
+        this.offset = offset;
+    }
+
+    /**
+     * Returns the discrete (does not continue past 360) value of the encoder in degrees
+     * @return Discrete value of encoder in degrees
+     */
+    public double getAbsolutePositionDegrees () {
+        return encoder.getValue()*360+offset;
+    }
+
+    /**
+     * Returns the discrete (does not continue past 2pi) value of the encoder in radians
+     * @return Discrete value of encoder in radians
+     */
+    public double getAbsolutePositionRadians () {
+        return Units.degreesToRadians(getAbsolutePositionDegrees());
+    }
+
+    /**
+     * Returns the discrete (does not continue past 2pi) value of the encoder in radians
+     * @return Discrete value of encoder in radians
+     */
+    public Rotation2d getAbsolutePositionRotation2D () {
+        return new Rotation2d(Units.degreesToRadians(getAbsolutePositionDegrees()));
+    }
+}
